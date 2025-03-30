c2468805400d4c1fa4d8f20be5b7c03ac89c8909
youdy17 <youdy017@gmail.com>
2025-03-30 22:13:52 +0700

    test

--- exercise-3/src/components/OrderCard.jsx
+++ exercise-3/src/components/OrderCard.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 export default function OrderCard() {
+  const [product,setProduct]=useState=("");
+  const [price,setPrice] = useState("");
+  const [quantity,setQuantity] = useState(0);
+  const [total,setTotal] = useState(0);
+
+  function getOrderinfo({product,price,quantity}){
+    setProduct(product);
+    setPrice(price);
+    setQuantity(quantity);
+    setTotal(price*quantity);
+
+  }
+
   return (
     <div className="order">
       <div>
-        <h4>TODO NAME</h4>
-        <small>TODO PRICE</small>
+        <h4>{setProduct}</h4>
+        <small>{price}</small>
       </div>
 
       <div className="order-quantity">
+        {quantity}
         <div className="order-button">-</div>
-        <h4>TODO PRICE</h4>
+        <h4>{totalPrice}</h4>
         <div className="order-button">+</div>
       </div>
     </div>
